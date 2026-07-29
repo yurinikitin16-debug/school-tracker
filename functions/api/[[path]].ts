b88be553cd24da76ad6e658bc6d85da151fa5528
yurinikitin16-debug <https://github.com/yurinikitin16-debug>
2026-07-29 22:10:03 +0300

    Updated api path calls

---
+++ functions/api/[[path]].ts
@@ -0,0 +1,32 @@
+const DEFAULT_API_ORIGIN = 'https://school-tracker-api.yurinikitin16.workers.dev';
+
+interface PagesFunctionContext {
+  request: Request;
+  env: {
+    API_ORIGIN?: string;
+  };
+}
+
+export async function onRequest({ request, env }: PagesFunctionContext): Promise<Response> {
+  const requestUrl = new URL(request.url);
+  const apiOrigin = (env.API_ORIGIN || DEFAULT_API_ORIGIN).replace(/\/$/, '');
+  const targetUrl = new URL(`${requestUrl.pathname}${requestUrl.search}`, apiOrigin);
+  const headers = new Headers(request.headers);
+
+  headers.delete('host');
+
+  const response = await fetch(
+    new Request(targetUrl.toString(), {
+      method: request.method,
+      headers,
+      body: request.body,
+      redirect: 'manual',
+    }),
+  );
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
+  });
+}
